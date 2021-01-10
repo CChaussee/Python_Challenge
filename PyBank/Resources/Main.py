b22b3d2e8c52c93952ace373fb82d6f9ff449d15
Chelsea Chaussee <cfchaussee@gmail.com>
2021-01-10 12:32:33 -0800

    net total attempt 1

--- PyBank/Resources/Main.py
+++ PyBank/Resources/Main.py
@@ -19,6 +19,7 @@ with open(csvpath) as csvfile:
 #Loop, The total number of months included in the dataset
     for row in csvreader:
         total_months = total_months + 1
+        net_total = net_total + 1
 
         
 
@@ -39,8 +40,8 @@ with open(csvpath) as csvfile:
 
 print("Financial Analysis")
 print("------------------")
-print("Total Months", total_months)
-#print("Total")
-#print("Average Change")
-#print("Greatest Increase in Profits")
-#print("Greatest Decrease in Profits"
+print("Total Months:", total_months)
+print("Total:", net_total)
+#print("Average Change:")
+#print("Greatest Increase in Profits:")
+#print("Greatest Decrease in Profits:"
