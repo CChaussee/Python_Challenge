a254da7550a19d1226c2f01a4506a3375562f5c1
Chelsea Chaussee <cfchaussee@gmail.com>
2021-01-22 16:08:34 -0800

    index is wrong

--- PyPoll/Resources/Main.py
+++ PyPoll/Resources/Main.py
@@ -9,8 +9,10 @@ Candidates = []
 Voter_count = []
 #defining variables
 votes = 0
-percent_votes = []
- 
+kvotes = 0
+cvotes = 0
+livotes = 0
+ovotes = 0 
 #read csv path
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
@@ -18,48 +20,79 @@ with open(csvpath) as csvfile:
 
 #loop
     for row in csvreader:
+#The total number of votes cast 
         votes = votes + 1
-        if row[2] not in Candidates:
-            Candidates.append(row[2])
-            Voter_count.append(1)
-            index = Candidates.index(row[2])
-        else:
-            index = Candidates.index(row[2])
-            Voter_count[index] + 1
+        Candidates.append(row[2])
+        Voter_count.append(row[1])
+            
+#The total number of votes each candidate won   
+total_k = Candidates.count("Khan")
+total_c = Candidates.count("Correy")
+total_li = Candidates.count("Li")
+total_o = Candidates.count("O'Tolley") 
+#The percentage of votes each candidate won
+percent_votesk = Candidates.count("Khan")/ votes * 100
+percent_votesc = Candidates.count("Correy")/ votes * 100
+percent_votesli = Candidates.count("Li")/ votes *100
+percent_voteso = Candidates.count("O'Tolley")/ votes *100 
+ 
+        
+#The winner of the election based on popular vote.            
 winner = max(Voter_count)        
-#trying to make my voter count list into an interger, thanks Google
-#tester1 = Voter_count
-#stringit = ''.join(tester1)
-#print(stringit)
-#percent_votes =  (votes/(Voter_count)*100)
 
 
 
-#stackoverflow to learn how to format percentage
+
+
+
+
+
+
+
+
+
+
+
+
+
 print("Election Results")
 print("--------------------")
 print("Total Votes:", votes)
 print("--------------------")
-print(Candidates[0], ":", percent_votes)
-print(Candidates[1], ":", percent_votes)
-print(Candidates[2], ":", percent_votes)
-print(Candidates[3], ":", percent_votes)
+print(total_k), ":", (percent_votesk)
+print(total_c), ":", (percent_votesc)
+print(total_li), ":", (percent_votesli)
+print(total_o), ":", (percent_voteso)
 print("--------------------")
 print("Winner :", winner) 
 print("--------------------")
 
 
+#Election Results
+#-------------------------
+#Total Votes: 3521001
+#-------------------------
+#Khan: 63.000% (2218231)
+#Correy: 20.000% (704200)
+#Li: 14.000% (492940)
+#O'Tooley: 3.000% (105630)
+#-------------------------
+#Winner: Khan
+#-------------------------
+
+
+
 #write to .txt file, there has to be an easier way to do this
-with open(analysis.txt ,'w') as analysis.txt:
-    with open(csvpath, 'w') as csvbook:
-    print("Election Results", file=csvbook)
-    print("--------------------",file=csvbook)
-    print("Total Votes:", votes, file=csvbook)
-    print("--------------------", file=csvbook)
-    print(Candidates[0], ":",file=csvbook)
-    print(Candidates[1], ":", ),file=csvbook)
-    print(Candidates[2], ":", file=csvbook)
-    print(Candidates[3], ":", file=csvbook)
-    print("--------------------",file=csvbook)
-    print("Winner :", winner,file=csvbook)
-    print("--------------------",file=csvbook) 
+#with open(analysis.txt ,'w') as analysis.txt:
+    #with open(csvpath, 'w') as csvbook:
+    #print("Election Results", file=csvbook)
+    #print("--------------------",file=csvbook)
+    #print("Total Votes:", votes, file=csvbook)
+    #print("--------------------", file=csvbook)
+    #print(Candidates[0], ":",file=csvbook)
+    #print(Candidates[1], ":", ),file=csvbook)
+    #print(Candidates[2], ":", file=csvbook)
+    #print(Candidates[3], ":", file=csvbook)
+    #print("--------------------",file=csvbook)
+    #print("Winner :", winner,file=csvbook)
+    #print("--------------------",file=csvbook) 
